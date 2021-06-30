256784f6cedd6cafa330692405c4b20a2c5d99c2
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2021-06-30 20:49:59 +1000

    Finish accordion project

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = (props) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{props.data.title}</h4>
+        <button className="btn" onClick={() => setOpen(!open)}>
+          {open ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {open ? <p>{props.data.info}</p> : null}
+    </article>
+  );
 };
 
 export default Question;
