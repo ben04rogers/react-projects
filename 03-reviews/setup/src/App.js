95dcbe682c60f5d1dafa05fc60837c339cc68b3b
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2021-06-30 11:28:22 +1000

    Finish tours project

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Review from "./Review";
-import { FaGithubSquare } from "react-icons/fa";
 function App() {
   return (
-    <h2>
-      reviews project setup
-      <FaGithubSquare />
-    </h2>
+    <main>
+      <div className="title">
+        <h2>our reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </main>
   );
 }
 
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState(people);
+  console.log(data);
+  return <article></article>;
 };
 
 export default Review;
